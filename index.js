6063a02da3a4a50489842fc1056e2f64ced81e73
g-barbosa <giovanne.bbarbosa@gmail.com>
2021-03-20 18:27:43 -0300

    refactor: return just data from response

--- index.js
+++ index.js
@@ -4,25 +4,19 @@ class PROCONAPI {
 
   constructor(uri, token) {
     this.uri = uri
-    this.token = token
+    this.headers = { 'Authorization': `Bearer ${token}` }
   }
 
   async GetComplaints ( year, limit, offset ) {
-    axios.get(this.uri + year, {
+    var complaints = await axios.get(this.uri + year, {
       params: {
         limite: limit === undefined ? 20 : limit,
         offset: offset === undefined ? 1 : offset
       },
-      headers: {
-        'Authorization': this.token
-      }
+      headers: this.headers
     })
-    .then((data) => {
-      console.log(data.data)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+
+    return complaints.data
   } 
 }
 
